5bd711e04ddfa6ea2e2b8a6a435e8c18d4feb81d
ginil <ginil@diginestsolutions.in>
2024-01-29 13:06:13 +0530

    update

---
+++ src/components/CustomSearch.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CustomSearch = () => {
+  return (
+    <View>
+      <Text>CustomSearch</Text>
+    </View>
+  )
+}
+
+export default CustomSearch
+
+const styles = StyleSheet.create({})
