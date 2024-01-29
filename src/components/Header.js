95cac62ace4f6fc4f3fc132c648f15acd2b3a28d
ginil <ginil@diginestsolutions.in>
2024-01-29 14:45:35 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React, { memo } from 'react'
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Header = memo(() => {
     return (
         <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image source={require('../images/DG.png')} style={styles.logo} />
+            <View style={styles.imageContainer}>
+                <Image source={require('../images/DG.png')} style={styles.logo} />
+            </View>
+            <View style={styles.iconContainer}>
+                <TouchableOpacity>
+                    <FontAwesome name='shopping-cart' size={20} color="#000" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontAwesome name='bell' size={20} color="#000" />
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={styles.iconContainer}>
-          <FontAwesome name='shopping-cart' size={20} color="#000" />
-          <FontAwesome name='bell' size={20} color="#000" />
-        </View>
-      </View>
     )
 })
 
@@ -23,21 +27,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row', // Arrange children in a row
         justifyContent: 'space-between', // Space between children
         alignItems: 'center', // Align items in the center vertically
-        paddingHorizontal: 20, 
-        height:60
-      },
-      imageContainer: {
-        marginRight: 10, // Margin to separate the image from the icon
-      },
-      iconContainer: {
-        display:'flex',
-        flexDirection:'row',
-        gap:10,
-        marginLeft: 10, // Margin to separate the icon from the image
-      },
-      logo: {
-        width: 100, // Set the width of the image
-        height: 100, // Set the height of the image
-        resizeMode: 'contain', // Ensure the image maintains its aspect ratio
-      },
+        paddingHorizontal: 22,
+        height: 60
+    },
+    imageContainer: {
+        marginRight:10, // Margin to separate the image from the icon
+    },
+    iconContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+       // Margin to separate the icon from the image
+    },
+    logo: {
+        width: 120, // Set the width of the image
+        height: 120, // Set the height of the image
+        // resizeMode: 'contain', // Ensure the image maintains its aspect ratio
+    },
 })
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from '../../components/Header'
+import CustomSearch from '../../components/CustomSearch'
 
 const Home = () => {
   return (
     <View>
       <Header/>
+      <CustomSearch/>
     </View>
   )
 }
