b688c4e811dabb755b5d39593d1868a193bbe542
Dipin <dipin@diginestsolutions.in>
2024-02-15 12:10:49 +0530

    Nw10

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,6 +1,6 @@
 
 export const GOOGLE_API = 'AIzaSyBBcghyB0FvhqML5Vjmg3uTwASFdkV8wZY'
 
-export const BASE_URL = 'https://digistoreapi.diginestsolutions.in/'
+export const BASE_URL = 'https://digifashionapi.diginestsolutions.in/'
 
 export const RAZORPAY_KEY= 'rzp_test_pUZiRv4OpzV68n'
