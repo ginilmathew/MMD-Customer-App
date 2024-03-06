a604bbf90b2a966f87dfc3e1808f96bc459ddcea
Nazimudheen <nazimudheen@diginestsolutions.in>
2024-03-06 09:49:42 +0530

    code updated

--- src/constants/API.js
+++ src/constants/API.js
@@ -2,7 +2,7 @@
 export const GOOGLE_API = 'AIzaSyBBcghyB0FvhqML5Vjmg3uTwASFdkV8wZY'
 
 
-export const env = "dev"
+export const env = "fashion"
 
 const URLS = {
     fashion: "https://digifashionapi.diginestsolutions.in/",
